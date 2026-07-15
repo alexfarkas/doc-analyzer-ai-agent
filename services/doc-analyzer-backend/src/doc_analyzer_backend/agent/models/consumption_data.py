6c28ad8ba72eea4e2aa23321f0163c8dc0323304
Вовк Алексей Анатольевич <avovk@samokat.ru>
2026-07-15 19:37:19 +0300

    rft(doc-analyzer-backend): consumption data with tokens, elapsed, cost

---
+++ services/doc-analyzer-backend/src/doc_analyzer_backend/agent/models/consumption_data.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+
+from src.doc_analyzer_backend.llm.tokens.token_usage import TokenUsage, create_token_usage
+
+
+class ConsumptionData(BaseModel):
+    token_usage: TokenUsage | None = None
+    elapsed: float= 0.0
+    cost: float = 0.0
+
+    def update_by_data(self, data: ConsumptionData):
+        self.token_usage.add_usage(data.token_usage)
+        self.elapsed += data.elapsed
+        self.cost += data.cost
+
+    def update(self, token_usage: TokenUsage, elapsed: float, cost: int):
+        self.token_usage.add_usage(token_usage)
+        self.elapsed += elapsed
+        self.cost += cost
+
+
+def create_consumption_data(
+    token_usage: TokenUsage | None = None,
+    elapsed: float = 0.0,
+    cost: float = 0.0,
+) -> ConsumptionData:
+    if token_usage is None:
+        token_usage = create_token_usage()
+    return ConsumptionData(
+        token_usage=token_usage,
+        elapsed=elapsed,
+        cost=cost,
+    )
---
+++ services/doc-analyzer-backend/src/doc_analyzer_backend/llm/tokens/total_tokens_cost_utils.py
@@ -0,0 +1,24 @@
+import logging
+
+from src.doc_analyzer_backend.agent.models.consumption_data import ConsumptionData
+from src.doc_analyzer_backend.data.app_state_manager import app_state
+from src.doc_analyzer_backend.llm.tokens.token_usage import TokenUsage
+
+logger = logging.getLogger(__name__)
+
+
+async def update_total_consumption(
+    consumption_data: ConsumptionData,
+) -> tuple[TokenUsage, float]:
+    logger.info(f"Updating total consumption data with: {consumption_data}")
+
+    await app_state.add_token_usage(consumption_data.token_usage)
+    await app_state.add_cost(consumption_data.cost)
+
+    total_token_usage = await app_state.get_token_usage()
+    total_cost = await app_state.get_cost()
+
+    logger.info(f"Total token usage: {total_token_usage}")
+    logger.info(f"Total cost: {total_cost}")
+
+    return total_token_usage, total_cost
