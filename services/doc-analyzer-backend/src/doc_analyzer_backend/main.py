30cbcf9f79974b905d5d44fd98fe329401d4663b
Вовк Алексей Анатольевич <avovk@samokat.ru>
2026-08-06 16:35:27 +0300

    fix(doc-analyzer-backend): app launch

--- services/doc-analyzer-backend/src/doc_analyzer_backend/main.py
+++ services/doc-analyzer-backend/src/doc_analyzer_backend/main.py
@@ -5,10 +5,11 @@ from src.doc_analyzer_backend.config.loader.settings import app_settings
 if __name__ == "__main__":
     service_config = app_settings().service
     uvicorn.run(
-        "api.api:app",
+        "src.doc_analyzer_backend.api.api:app",
         host=service_config.host,
         port=service_config.port,
         timeout_keep_alive=service_config.timeout_keep_alive,
         timeout_graceful_shutdown=service_config.timeout_graceful_shutdown,
         reload=service_config.reload,
+        reload_dirs=["src"],
     )
